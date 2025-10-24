fa405cfebdf57661463945dc2ddadc95f018116b
Rizki Syafa <safanirmala0907@gmail.com>
2025-10-24 17:08:50 +0700

    uts final

--- daftar.php
+++ daftar.php
@@ -26,11 +26,6 @@
             animation: fadeIn 0.6s ease-in-out;
         }
 
-        @keyframes fadeIn {
-            from { opacity: 0; transform: translateY(10px); }
-            to { opacity: 1; transform: translateY(0); }
-        }
-
         h1 {
             text-align: center;
             color: #222;
